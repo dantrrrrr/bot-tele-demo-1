4fb75e6ecab41f2592b883cec53ab9c2073b90fc
dantr <huynhngdantr@gmail.com>
2024-07-29 10:07:37 +0700

    test2

--- api/bot.js
+++ api/bot.js
@@ -1,10 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with the token you received from BotFather
 const token = process.env.TOKEN;
+const url = process.env.URL;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
+
+const app=express();
+app.use(bodyParser.json());
+
+bot.setWebHook(`${url}/api/bot`);
+
+app.post('/api/bot',(req,res)=>{
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+
+})
 
 // Matches any message
 bot.on('message', (msg) => {
@@ -16,3 +32,4 @@ bot.on('message', (msg) => {
 });
 
 console.log('Bot is running...');
+module.exports=app;
